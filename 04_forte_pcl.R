035afe90fd57302add03a6eba470b8be72bd6a5a
Jeff Atkins <jwatkins6@vcu.edu>
2020-02-13 15:39:10 -0500

    updating some old scriptsd

--- 04_forte_pcl.R
+++ 04_forte_pcl.R
@@ -4,6 +4,8 @@ require(dplyr)
 require(ggplot2)
 require(tidyverse)
 
+##############2018
+
 pcl <- read.csv("./data/forte_pcl_2018.csv")
 
 head(pcl)
@@ -20,6 +22,8 @@ pcl %>%
 #
 df <- data.frame(df)
 
+
+write.csv(df, "rugosity_shorty.csv")
 
 #boxplot rugosity
 # A basic box with the conditions colored
@@ -48,5 +52,49 @@ ggplot(pcl, aes(x = group, y = sky.fraction, fill = group))+
   ylab("Gap Fraction")
 
 
+
+###### 2019
+
+require(forestr)
+
+
+# pcl work
+require(forestr)
+
+
+# lines check
+#this script, thanks in part to the indomitable Ben Bond-Lamberty, error checks which files have missing lines.
+og <- getwd()
+setwd("./data/pcl/pcl2019")
+
+files <- list.files(pattern = "*.CSV", full.names = TRUE)
+
+for(i in seq_along(files)) {
+  filedata <- readLines(files[i])
+  lines_to_skip <- min(which(filedata != "")) - 1
+  cat(i, files[i], lines_to_skip, "\n")
+  x <- read.csv(files[i], skip = lines_to_skip)
+}
+
+setwd(og)
+
+
+# run them bois
+data_dir <- "./data/pcl/pcl2019"
+process_multi_pcl(data_dir, marker.spacing = 10, user_height = 1.1)
+
+
+## set your output directory
+output_directory <- "./output/output/"
+
+#
+library(dplyr)
+library(readr)
+df <- list.files(path = output_directory, full.names = TRUE) %>%
+  lapply(read_csv) %>%
+  bind_rows
+
+df <- data.frame(df)
+write.csv(df, "hmc_pcl.CSV")
 
 
